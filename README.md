ca6f4ee78b1d98adec7e5db36539677de1e4acfd
David <157735322+David735453@users.noreply.github.com>
2025-09-11 20:21:57 +0300

    add Website

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hausaufgaben-10G3
+
+Website für Klasse 10G3
+
+(besser gesagt für wer hausi vergisst)
---
+++ utils/global-data.js
@@ -0,0 +1,17 @@
+export const getGlobalData = () => {
+  const name = process.env.BLOG_NAME
+    ? decodeURI(process.env.BLOG_NAME)
+    : 'Viel Spaß beim Bruch!';
+  const blogTitle = process.env.BLOG_TITLE
+    ? decodeURI(process.env.BLOG_TITLE)
+    : 'Hausaufgaben 10G3';
+  const footerText = process.env.BLOG_FOOTER_TEXT
+    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
+    : 'Weder ich noch jemand der mir etwas zuschickt verspricht, dass diese Angaben auch nur im geringsten richtig sind und über nimmt keine Folgen für diese Fehler.';
+
+  return {
+    name,
+    blogTitle,
+    footerText,
+  };
+};
